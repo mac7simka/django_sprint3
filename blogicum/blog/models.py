6cb4050e1ec4169e6572d18bf9db96b6f7f862f5
Maksim Karaskin <mac7sim@gmail.com>
2024-03-25 15:49:22 +0300

    первая отправка на ревью

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -12,7 +12,9 @@ class Post(PublishedModel, CreatedModel):
     pub_date = models.DateTimeField(
         auto_now=False,
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
+        help_text=(
+            'Если установить дату и время в будущем — можно '
+            'делать отложенные публикации.'))
 
     author = models.ForeignKey(
         User,
@@ -50,7 +52,8 @@ class Category(PublishedModel, CreatedModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
+        help_text=('Идентификатор страницы для URL; разрешены символы '
+                   'латиницы, цифры, дефис и подчёркивание.'))
 
     class Meta:
         verbose_name = 'категория'
